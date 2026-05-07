f8db424ec7215e9157c531ce8743f5357e1b30c1
Aishwaryaiitj1004 <g25ai1004@iitj.ac.in>
2026-05-07 16:40:55 +0530

    app.py

--- app.py
+++ app.py
@@ -1,26 +1,97 @@
-import streamlit as st
 import pandas as pd
 
-uploaded_file = st.file_uploader(
-    "Upload Excel",
-    type=["xls", "xlsx"]
-)
-
-if uploaded_file:
+def process_excel(uploaded_file):
 
     filename = uploaded_file.name.lower()
 
+    # READ XLSX
     if filename.endswith(".xlsx"):
+
         df = pd.read_excel(
             uploaded_file,
             header=4,
             engine="openpyxl"
         )
+
+    # READ XLS
     else:
+
         df = pd.read_excel(
             uploaded_file,
             header=4,
             engine="xlrd"
         )
 
-    st.write(df.columns.tolist())
+    # FORCE UNIQUE COLUMN NAMES
+    df.columns = [
+        "Group",
+        "Operation",
+        "Operator_No",
+        "Operator_Name",
+        "WIP",
+        "SAM",
+        "Target",
+        "Machine_Type",
+        "Pcs_1",
+        "Pcs_2",
+        "Pcs_3",
+        "Pcs_4",
+        "Pcs_5",
+        "Pcs_6",
+        "Pcs_7",
+        "Pcs_8",
+        "Pcs_9",
+        "Total_Pcs",
+        "EM",
+        "AM",
+        "IDT",
+        "NWT",
+        "RWT",
+        "TDT",
+        "Prodn",
+        "Eff"
+    ]
+
+    # KEEP ONLY IMPORTANT COLUMNS
+    df = df[
+        [
+            "Operation",
+            "Operator_Name",
+            "SAM",
+            "Target",
+            "IDT",
+            "NWT",
+            "RWT",
+            "TDT",
+            "Eff"
+        ]
+    ]
+
+    # REMOVE EMPTY ROWS
+    df = df[
+        df["Operation"].notna()
+    ]
+
+    # NUMERIC CONVERSION
+    numeric_cols = [
+        "IDT",
+        "NWT",
+        "RWT",
+        "TDT",
+        "Eff"
+    ]
+
+    for col in numeric_cols:
+
+        df[col] = pd.to_numeric(
+            df[col],
+            errors="coerce"
+        ).fillna(0)
+
+    # ONLY CONSTRAINT + BEFORE CONSTRAINT
+    filtered_df = df[
+        (df["Eff"] <= 10) |
+        (df["NWT"] >= 350)
+    ]
+
+    return filtered_df
